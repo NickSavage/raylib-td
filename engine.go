90f5eb7a687ff5a9ae1a5c5de5a53fe550a81af8
Nick Savage <nick@nicksavage.ca>
2024-10-10 19:33:05 -0400

    add image

---
+++ engine.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"github.com/gen2brain/raylib-go/raylib"
+)
+
+type Button struct {
+	Rectangle rl.Rectangle
+	Color     rl.Color
+	Text      string
+	TextColor rl.Color
+	OnClick   func(*Game)
+}
+type Scene struct {
+	Name        string
+	Active      bool
+	AutoDisable bool
+	DrawScene   func(*Game)
+	UpdateScene func(*Game)
+	Buttons     []Button
+	skip        bool
+	Data        map[string]interface{}
+}
+
+func (g *Game) ActivateScene(sceneName string) {
+	for key, scene := range g.Scenes {
+		if key == sceneName {
+			scene.Active = true
+		} else if scene.AutoDisable {
+			scene.Active = false
+		} else {
+			// do nothing
+		}
+		g.Scenes[key] = scene
+	}
+}
+
+func (g *Game) DrawButtons(buttons []Button) {
+	for _, button := range buttons {
+		rl.DrawRectangle(button.Rectangle.ToInt32().X, button.Rectangle.ToInt32().Y, button.Rectangle.ToInt32().Width, button.Rectangle.ToInt32().Height, button.Color)
+		rl.DrawText(
+			button.Text,
+			button.Rectangle.ToInt32().X,
+			button.Rectangle.ToInt32().Y,
+			10,
+			button.TextColor,
+		)
+	}
+
+}
+
+func (g *Game) WasButtonClicked(button *Button) bool {
+	if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+		mousePosition := rl.GetMousePosition()
+		if rl.CheckCollisionPointRec(mousePosition, button.Rectangle) {
+			return true
+		}
+	}
+	return false
+}
+
+func (g *Game) Draw() {
+
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.White)
+	for _, scene := range g.Scenes {
+		if !scene.Active {
+			continue
+		}
+		scene.DrawScene(g)
+	}
+	rl.EndDrawing()
+}
+
+func (g *Game) Update() {
+	if rl.IsKeyPressed(rl.KeyEscape) {
+		if g.Scenes["Pause"].Active {
+			g.ActivateScene("Round")
+		} else {
+			g.ActivateScene("Pause")
+		}
+	}
+	for _, scene := range g.Scenes {
+		if !scene.Active {
+			continue
+		}
+		scene.UpdateScene(g)
+	}
+}
--- main.go
+++ main.go
@@ -14,14 +14,12 @@ const GOLD_INCREASE_RATE float32 = 0.1
 
 type Game struct {
 	Scenes       map[string]*Scene
+	GameData     map[string]interface{}
 	player       Player
 	enemies      []Enemy
 	aliveEnemies int
-	paused       bool
 	screenWidth  int32
 	screenHeight int32
-	shopMenuOpen bool
-	placeTurret  bool
 }
 
 type Player struct {
@@ -37,6 +35,7 @@ type Enemy struct {
 	HealthBar rl.Rectangle
 	Speed     int
 	Alive     bool
+	Sprite    rl.Texture2D
 }
 
 type Turret struct {
@@ -44,58 +43,32 @@ type Turret struct {
 	Color     rl.Color
 	Range     int
 	Damage    int
+	FireRate  float32
+	Cost      int
 }
 
 // gui stuff
 
-type Button struct {
-	Rectangle rl.Rectangle
-	Color     rl.Color
-	Text      string
-	TextColor rl.Color
-	OnClick   func(*Game)
-}
-type Scene struct {
-	Name        string
-	Active      bool
-	AutoDisable bool
-	DrawScene   func(*Game)
-	UpdateScene func(*Game)
-	Buttons     []Button
-	skip        bool
+var TURRET_DATA map[string]Turret
+var ENEMY_DATA map[string]Enemy
+
+func LoadEnemyData() map[string]Enemy {
+
+	image := rl.LoadImage("assets/sprite.png")
+	sprite := rl.LoadTextureFromImage(image)
+	results := make(map[string]Enemy)
+	results["Normal"] = Enemy{Color: rl.Red, Health: 100, Speed: 2, Sprite: sprite}
+	results["Fast"] = Enemy{Color: rl.Green, Health: 100, Speed: 5, Sprite: sprite}
+	results["Buff"] = Enemy{Color: rl.Yellow, Health: 200, Speed: 2, Sprite: sprite}
+	return results
 }
 
-func (g *Game) ActivateScene(sceneName string) {
-	for key, scene := range g.Scenes {
-		if key == sceneName {
-			scene.Active = true
-		} else if scene.AutoDisable {
-			scene.Active = false
-		} else {
-			// do nothing
-		}
-		g.Scenes[key] = scene
-	}
-}
-
-func (g *Game) WasButtonClicked(button *Button) bool {
-	if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
-		mousePosition := rl.GetMousePosition()
-		if rl.CheckCollisionPointRec(mousePosition, button.Rectangle) {
-			return true
-		}
-	}
-	return false
-}
-
-var ENEMY_DATA []Enemy
-
-func LoadEnemyData() []Enemy {
-	results := []Enemy{
-		Enemy{Color: rl.Red, Health: 100, Speed: 2},
-		Enemy{Color: rl.Green, Health: 100, Speed: 5},
-		Enemy{Color: rl.Yellow, Health: 200, Speed: 2},
-	}
+func LoadTurretData() map[string]Turret {
+	results := make(map[string]Turret)
+	results["Basic"] = Turret{Range: 50, Damage: 5, FireRate: 1, Cost: 50}
+	results["Strong"] = Turret{Range: 30, Damage: 7, FireRate: 0.5, Cost: 100}
+	results["Wide"] = Turret{Range: 100, Damage: 3, FireRate: 1, Cost: 100}
+	results["Fast"] = Turret{Range: 100, Damage: 2, FireRate: 2, Cost: 100}
 	return results
 }
 
@@ -103,20 +76,21 @@ func CreateRoundOneEnemies(startX, startY float32) []Enemy {
 	results := []Enemy{}
 	var enemy Enemy
 	for i := range 10 {
-		enemy = CreateEnemy(0, float32(i*-40), float32(startY/2))
+		enemy = CreateEnemy("Normal", float32(i*-60), float32(startY/2))
 		results = append(results, enemy)
 	}
-	enemy = CreateEnemy(2, float32(len(results)*-40), float32(startY/2))
+	enemy = CreateEnemy("Fast", float32(len(results)*-60), float32(startY/2))
 	results = append(results, enemy)
 	return results
 
 }
 
-func CreateEnemy(enemyType int, x, y float32) Enemy {
+func CreateEnemy(enemyType string, x, y float32) Enemy {
 	result := ENEMY_DATA[enemyType]
 	result.Rectangle = rl.Rectangle{X: x, Y: y, Width: 25, Height: 25}
-	result.HealthBar = rl.Rectangle{X: x, Y: y - 10, Width: 25, Height: 5}
+	result.HealthBar = rl.Rectangle{X: x, Y: y - 10, Width: 48, Height: 5}
 	result.Alive = true
+	log.Printf("sprite %v", result.Sprite)
 	return result
 }
 
@@ -157,7 +131,6 @@ func (g *Game) checkHits(turret Turret) {
 		) {
 			g.enemies[i].Health -= turret.Damage
 			hasShot = true
-			log.Printf("taking damage! %v", g.enemies[i].Health)
 
 		}
 		if g.enemies[i].Health <= 0 {
@@ -169,7 +142,7 @@ func (g *Game) checkHits(turret Turret) {
 
 func (enemy *Enemy) Move() {
 	enemy.Rectangle.X = enemy.Rectangle.X + float32(enemy.Speed*1)
-	enemy.HealthBar.Width = float32(enemy.Health / 4)
+	enemy.HealthBar.Width = float32(enemy.Health / 2)
 	enemy.HealthBar.X = enemy.HealthBar.X + float32(enemy.Speed*1)
 }
 
@@ -180,6 +153,22 @@ func OnClickOpenShopButton(g *Game) {
 func OnClickShopBasicTurret(g *Game) {
 	g.Scenes["PlaceTurrets"].skip = true
 	g.ActivateScene("PlaceTurrets")
+	g.Scenes["PlaceTurrets"].Data["ChosenTurret"] = TURRET_DATA["Basic"]
+}
+func OnClickShopFastTurret(g *Game) {
+	g.Scenes["PlaceTurrets"].skip = true
+	g.ActivateScene("PlaceTurrets")
+	g.Scenes["PlaceTurrets"].Data["ChosenTurret"] = TURRET_DATA["Fast"]
+}
+func OnClickShopStrongTurret(g *Game) {
+	g.Scenes["PlaceTurrets"].skip = true
+	g.ActivateScene("PlaceTurrets")
+	g.Scenes["PlaceTurrets"].Data["ChosenTurret"] = TURRET_DATA["Strong"]
+}
+func OnClickShopWideTurret(g *Game) {
+	g.Scenes["PlaceTurrets"].skip = true
+	g.ActivateScene("PlaceTurrets")
+	g.Scenes["PlaceTurrets"].Data["ChosenTurret"] = TURRET_DATA["Wide"]
 }
 
 func InitPlayer() Player {
@@ -205,15 +194,26 @@ func DrawRound(g *Game) {
 	}
 	for i := range g.enemies {
 		if g.enemies[i].Alive {
-			rl.DrawRectangleRec(g.enemies[i].Rectangle, g.enemies[i].Color)
+			rl.DrawTextureRec(
+				g.enemies[i].Sprite,
+				rl.Rectangle{
+					X:      0,
+					Y:      0,
+					Width:  48,
+					Height: 48,
+				},
+				rl.Vector2{
+					X: g.enemies[i].Rectangle.X,
+					Y: g.enemies[i].Rectangle.Y,
+				},
+				g.enemies[i].Color)
+			//			rl.DrawRectangleRec(g.enemies[i].Rectangle, g.enemies[i].Color)
 			rl.DrawRectangleRec(g.enemies[i].HealthBar, rl.Red)
 		}
 	}
 }
 
 func UpdateRound(g *Game) {
-	log.Printf("update round")
-
 	g.player.Gold += (1 * GOLD_INCREASE_RATE)
 
 	for i := range g.player.Turrets {
@@ -261,6 +261,7 @@ func UpdateHUD(g *Game) {
 			g.Scenes["HUD"].Buttons[0].OnClick(g)
 		}
 	}
+	g.DrawButtons(g.Scenes["HUD"].Buttons)
 }
 func DrawPause(g *Game) {
 	log.Printf("?")
@@ -274,21 +275,36 @@ func UpdatePause(g *Game) {
 
 func DrawShop(g *Game) {
 	rl.DrawText("Shop", g.screenWidth/2, 100, 20, rl.Black)
-
+	g.DrawButtons(g.Scenes["Shop"].Buttons)
 }
 func UpdateShop(g *Game) {
 	if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
-		if g.WasButtonClicked(&g.Scenes["Shop"].Buttons[0]) {
-			g.Scenes["Shop"].Buttons[0].OnClick(g)
+		for _, button := range g.Scenes["Shop"].Buttons {
+			if g.WasButtonClicked(&button) {
+				button.OnClick(g)
+			}
+
 		}
 	}
 }
 
 func DrawPlaceTurret(g *Game) {
 	DrawRound(g)
+
+	var chosenTurret Turret
+	if data, ok := g.Scenes["PlaceTurrets"].Data["ChosenTurret"]; ok {
+		log.Printf("chosen turret %v", data)
+		chosenTurret, _ = data.(Turret)
+	}
 	mousePosition := rl.GetMousePosition()
 
-	rl.DrawCircle(int32(mousePosition.X), int32(mousePosition.Y), 50, rl.LightGray)
+	log.Printf("chosen %v", chosenTurret)
+	rl.DrawCircle(
+		int32(mousePosition.X),
+		int32(mousePosition.Y),
+		float32(chosenTurret.Range),
+		rl.LightGray,
+	)
 
 }
 
@@ -304,54 +320,13 @@ func UpdatePlaceTurret(g *Game) {
 		g.ActivateScene("Round")
 	}
 }
-
-func (g *Game) Draw() {
-
-	rl.BeginDrawing()
-	rl.ClearBackground(rl.White)
-	for _, scene := range g.Scenes {
-		if !scene.Active {
-			continue
-		}
-		scene.DrawScene(g)
-		for _, button := range scene.Buttons {
-			rl.DrawRectangle(button.Rectangle.ToInt32().X, button.Rectangle.ToInt32().Y, button.Rectangle.ToInt32().Width, button.Rectangle.ToInt32().Height, button.Color)
-			rl.DrawText(
-				button.Text,
-				button.Rectangle.ToInt32().X,
-				button.Rectangle.ToInt32().Y,
-				10,
-				button.TextColor,
-			)
-		}
-	}
-	rl.EndDrawing()
-}
-
-func (g *Game) Update() {
-	if rl.IsKeyPressed(rl.KeyEscape) {
-		if g.Scenes["Pause"].Active {
-			g.ActivateScene("Round")
-		} else {
-			g.ActivateScene("Pause")
-		}
-	}
-	for _, scene := range g.Scenes {
-		if !scene.Active {
-			continue
-		}
-		scene.UpdateScene(g)
-	}
-}
 func main() {
 
-	ENEMY_DATA = LoadEnemyData()
+	TURRET_DATA = LoadTurretData()
 	g := Game{
 		Scenes:       map[string]*Scene{},
 		player:       InitPlayer(),
-		enemies:      CreateRoundOneEnemies(float32(800/2), float32(450/2)),
 		aliveEnemies: 0,
-		paused:       false,
 		screenWidth:  int32(800),
 		screenHeight: int32(450),
 	}
@@ -383,7 +358,7 @@ func main() {
 	g.Scenes["HUD"].Buttons[0] = Button{
 		Rectangle: rl.Rectangle{X: float32(g.screenWidth) - 110, Y: 10, Width: 100, Height: 15},
 		Color:     rl.Blue,
-		Text:      "Test",
+		Text:      "Shop",
 		TextColor: rl.Black,
 		OnClick:   OnClickOpenShopButton,
 	}
@@ -397,7 +372,7 @@ func main() {
 		Active:      false,
 		AutoDisable: true,
 		DrawScene:   DrawShop,
-		Buttons:     make([]Button, 1),
+		Buttons:     make([]Button, 4),
 		UpdateScene: UpdateShop,
 	}
 	g.Scenes["Shop"].Buttons[0] = Button{
@@ -412,15 +387,55 @@ func main() {
 		TextColor: rl.Black,
 		OnClick:   OnClickShopBasicTurret,
 	}
+	g.Scenes["Shop"].Buttons[1] = Button{
+		Rectangle: rl.Rectangle{
+			X:      200,
+			Y:      100,
+			Width:  100,
+			Height: 30,
+		},
+		Color:     rl.SkyBlue,
+		Text:      "Create Fast Turret (100)",
+		TextColor: rl.Black,
+		OnClick:   OnClickShopFastTurret,
+	}
+	g.Scenes["Shop"].Buttons[2] = Button{
+		Rectangle: rl.Rectangle{
+			X:      200,
+			Y:      150,
+			Width:  100,
+			Height: 30,
+		},
+		Color:     rl.SkyBlue,
+		Text:      "Create Strong Turret (100)",
+		TextColor: rl.Black,
+		OnClick:   OnClickShopStrongTurret,
+	}
+	g.Scenes["Shop"].Buttons[3] = Button{
+		Rectangle: rl.Rectangle{
+			X:      200,
+			Y:      200,
+			Width:  100,
+			Height: 30,
+		},
+		Color:     rl.SkyBlue,
+		Text:      "Create Wide Turret (100)",
+		TextColor: rl.Black,
+		OnClick:   OnClickShopWideTurret,
+	}
 	g.Scenes["PlaceTurrets"] = &Scene{
 		Active:      false,
 		AutoDisable: true,
 		DrawScene:   DrawPlaceTurret,
 		UpdateScene: UpdatePlaceTurret,
+		Data:        make(map[string]interface{}),
 	}
-	g.aliveEnemies = len(g.enemies)
 
 	rl.InitWindow(g.screenWidth, g.screenHeight, "tower defence")
+
+	ENEMY_DATA = LoadEnemyData()
+	g.enemies = CreateRoundOneEnemies(float32(800/2), float32(450/2))
+	g.aliveEnemies = len(g.enemies)
 
 	rl.SetTargetFPS(60)
 	//	mousePosition := rl.Vector2{X: 0, Y: 0}
